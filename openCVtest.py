d429f88aa54eb7204ebd00d6a5e963fdcdaa5614
akhil-jha <akhil.jha6@gmail.com>
2020-01-06 11:21:21 +0530

    in progress....

---
+++ openCVtest.py
@@ -0,0 +1,14 @@
+import cv2
+
+img = cv2.imread('luke.jpg',0)
+imG = cv2.resize(img,(240,240))
+cv2.imshow('Luke', imG)
+
+flag=True
+while flag:
+    k = cv2.waitKey(0) & 0xFF
+    if k==32:
+        cv2.destroyAllWindows()
+        flag=False
+    else:
+        k = cv2.waitKey(0)
