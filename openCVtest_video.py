c2bd7bb003394f7595f73b9cc95748a552785ece
akhil-jha <akhil.jha6@gmail.com>
2020-01-06 14:31:33 +0530

    Capture pic on login

--- openCVtest_video.py
+++ openCVtest_video.py
@@ -5,8 +5,8 @@ vid = cv2.VideoCapture(0)
 while True:
     ret, frame = vid.read()
     cv2.imshow("Press space for screenshot or q to quit",frame)
-    if cv2.waitKey(24) == 32:
-        cv2.imwrite("screenshot.jpg",frame)
+    if cv2.waitKey(1):
+        cv2.imwrite("/home/akjha/Desktop/screenshot.jpg",frame)
         break
     if cv2.waitKey(24) == ord('q'):
         break
