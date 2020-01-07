05037e49d034bb5c88e8c3034d6f4ff7cffc3e23
akhil-jha <akhil.jha6@gmail.com>
2020-01-07 12:56:50 +0530

    little better

--- openCVtest_video.py
+++ openCVtest_video.py
@@ -11,9 +11,9 @@ while True:
     DateTime = str(datetime.datetime.now())
     frame = cv2.putText(frame, DateTime ,(0,50), font, 1, (0,0,0), 2)
     cv2.imshow("Press space for screenshot or q to quit",frame)
-    # if cv2.waitKey(1):
-    #     cv2.imwrite("/home/akjha/Desktop/screenshot.jpg",frame)
-    #     break
+    if cv2.waitKey(1):
+        cv2.imwrite("/home/akjha/Desktop/screenshot.jpg",frame)
+        break
     if cv2.waitKey(24) == ord('q'):
         break
 
